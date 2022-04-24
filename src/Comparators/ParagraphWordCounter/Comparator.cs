426ed2101c67c1706506574daf460616bef40821
kevdra97 <kevdra@alu.iesserpis.org>
2022-04-24 16:28:52 +0200

    Prueba errores Sync

--- src/Comparators/ParagraphWordCounter/Comparator.cs
+++ src/Comparators/ParagraphWordCounter/Comparator.cs
@@ -149,11 +149,6 @@ namespace DocumentPlagiarismChecker.Comparators.ParagraphWordCounter
                 cr.AddMatch(cr.Child.Matching);
                 cr.DetailsData.Add(new object[]{paragraphs[0], paragraphs[1], cr.Child.Matching});
             }
-<<<<<<< HEAD
-                //hola
-=======
-\\ Mierda de ED
->>>>>>> 8b51c66133c30b80d51f434aa137848dbb78f225
             return cr; 
         }
     }   
