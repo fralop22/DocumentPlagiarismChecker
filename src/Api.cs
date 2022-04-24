89468f95424d789cb85ab3fefc7e3994d4b99f30
kevdra97 <kevdra@alu.iesserpis.org>
2022-04-24 16:10:33 +0200

    Prueba errores Sync

--- src/Api.cs
+++ src/Api.cs
@@ -107,7 +107,7 @@ namespace DocumentPlagiarismChecker
         /// <returns>A set of Comparator's object types</returns>
         private static IEnumerable<Type> GetComparatorTypes()
         {   
-            //TODO: Select plugins using a configuration file.
+            //TODO: Selecst plugins using a configuration file.
             return typeof(App).Assembly.GetTypes().Where(x => x.BaseType.Name.Contains("BaseComparator") && !x.FullName.Contains("_template")).ToList();
         }
 
